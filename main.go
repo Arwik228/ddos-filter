c9503300f3c07e3122190ddb784a7674e37e822b
Arwik <den_lebedev_2018@list.ru>
2022-08-05 22:27:51 +0500

    доработка проверок

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"net/http/httputil"
@@ -10,19 +11,28 @@ import (
 	"firewall"
 )
 
+type server struct {
+	db *sql.DB
+}
+
 func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	url, _ := url.Parse(config.Server)
 	proxy := httputil.NewSingleHostReverseProxy(url)
 	proxy.ServeHTTP(w, r)
 }
 
-func requestHandler(w http.ResponseWriter, r *http.Request) {
-	if firewall.CheckConnection(w, r) {
+func (s *server) requestHandler(w http.ResponseWriter, r *http.Request) {
+	if firewall.CheckConnection(s.db, w, r) {
 		proxyHandler(w, r)
 	}
 }
 
 func main() {
-	http.HandleFunc("/", requestHandler)
+	db, _ := sql.Open("sqlite3", "./database/sqlite.db")
+
+	defer db.Close()
+	s := server{db: db}
+
+	http.HandleFunc("/", s.requestHandler)
 	http.ListenAndServe(fmt.Sprintf(":%d", config.ProxyPort), nil)
 }
--- src/firewall/firewall.go
+++ src/firewall/firewall.go
@@ -1,22 +1,19 @@
 package firewall
 
 import (
-
-	//	"encoding/hex"
+	"database/sql"
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"strings"
 	"text/template"
-
-	//	"time"
-
-	"database/sql"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 type ViewData struct {
-	Token string
+	Url string
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
@@ -29,29 +26,39 @@ func createToken(ln int) string {
 	return string(b)
 }
 
-func firewallTemplate(w http.ResponseWriter) {
-	/*token := createToken(18)
-	hash := sha256.Sum256([]byte(token))
+func firewallTemplate(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	token := createToken(24)
 
-	log.Printf(hex.EncodeToString(hash))
-	expire := time.Now().Add(1440 * time.Minute)
-	cookie := http.Cookie{Name: "token", Value: string(hash[:]), Path: "/", Expires: expire, MaxAge: 90000}
-	http.SetCookie(w, &cookie)
-	*/
-	data := ViewData{Token: "str"}
+	_, err := db.Exec("INSERT INTO firewall_tokens (token, address, created_at) VALUES (?, ?, CURRENT_TIMESTAMP);", token, getUserIpAddress(r))
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	newUrl, err := url.Parse(r.URL.String())
+	if err != nil {
+		log.Fatal(err)
+	}
+	values := newUrl.Query()
+	values.Set("firewall_token", token)
+	newUrl.RawQuery = values.Encode()
+
+	data := ViewData{Url: newUrl.String()}
 	tmpl, _ := template.ParseFiles("views/index.html")
 	tmpl.Execute(w, data)
 }
 
-func getUserIpAddress(r *http.Request) string {
+func getUserIpAddress(r *http.Request) string { //todo delete port
 	IPAddress := r.Header.Get("X-Real-Ip")
 	if IPAddress == "" {
 		IPAddress = r.Header.Get("X-Forwarded-For")
 	}
+
 	if IPAddress == "" {
 		IPAddress = r.RemoteAddr
 	}
-	return IPAddress
+
+	return strings.SplitN(IPAddress, ":", 2)[0]
 }
 
 func checkUserCookieToken(db *sql.DB, token string, ip string) bool {
@@ -60,29 +67,77 @@ func checkUserCookieToken(db *sql.DB, token string, ip string) bool {
 	}
 
 	var count int
-	err := db.QueryRow("SELECT count(*) as count  FROM access_tokens WHERE token = ? AND address = ?", token, ip).Scan(&count)
+	row := db.QueryRow("SELECT count(*) as count  FROM access_tokens WHERE token = ? AND address = ?", token, ip)
+	err := row.Scan(&count)
+
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 	}
 
 	if count == 1 {
 		return true
 	}
 
-	return true
+	return false
 }
 
-func CheckConnection(w http.ResponseWriter, r *http.Request) bool {
-	db, _ := sql.Open("sqlite3", "./../database/database.db")
-	// Отрабатывает для пользователей прошедших авторизацию
-	tokenAccess, _ := r.Cookie("token_access")
-	if checkUserCookieToken(db, tokenAccess.Value, getUserIpAddress(r)) {
+func checkUserQuery(db *sql.DB, w http.ResponseWriter, r *http.Request) bool {
+	token := r.FormValue("firewall_token")
+	ip := getUserIpAddress(r)
+
+	if token == "" {
+		return false
+	}
+
+	var id int
+	row := db.QueryRow("SELECT id FROM firewall_tokens WHERE token = ? AND address = ?", token, ip)
+	err := row.Scan(&id)
+
+	if err != nil {
+		log.Print(err)
+	}
+
+	if id > 0 {
+		token := createToken(24)
+		db.Exec("DELETE FROM firewall_tokens WHERE id = ?", id)
+		_, err := db.Exec("INSERT INTO access_tokens (token, address, created_at) VALUES (?, ?, CURRENT_TIMESTAMP);", token, ip)
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		cookie := http.Cookie{Name: "token_access", Path: "/", Value: token, MaxAge: 9000}
+		http.SetCookie(w, &cookie)
+
 		return true
 	}
 
-	//queryToken := r.URL.Query().Get("fire_token")
+	return false
+}
 
-	db.Close()
-	//firewallTemplate(w)
+func CheckConnection(db *sql.DB, w http.ResponseWriter, r *http.Request) bool {
+	var tokenAccess string
+	tokenAccessCookie, err := r.Cookie("token_access")
+
+	if err != nil {
+		tokenAccess = ""
+	} else {
+		tokenAccess = tokenAccessCookie.Value
+	}
+
+	// Проверяем валидность токена доступа
+	if checkUserCookieToken(db, tokenAccess, getUserIpAddress(r)) == true {
+		return true
+	} else if tokenAccess != "" {
+		cookie := http.Cookie{Name: "token_access", Path: "/", MaxAge: 0}
+		http.SetCookie(w, &cookie)
+	}
+
+	// Проверяем валидность токена проверки если он есть
+	if checkUserQuery(db, w, r) == true {
+		return true
+	}
+
+	firewallTemplate(db, w, r)
 	return false
 }
